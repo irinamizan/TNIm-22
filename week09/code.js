3388404ac1ad392d1181cd5600039ffbfd9347a3
irinamizan <125154498+irinamizan@users.noreply.github.com>
2023-04-26 19:57:12 +0300

    Update code.js

--- week09/code.js
+++ week09/code.js
@@ -21,12 +21,12 @@ function init()
 		const boxgeometry = new THREE.BoxGeometry( 1, 1, 1 );
 		
 		const boxmaterials = [
-						new THREE.MeshBasicMaterial( { map: loader.load(  '/assets/cube1.png' ) } ),
-						new THREE.MeshBasicMaterial( { map: loader.load(  '/assets/cube2.png' ) } ),
-						new THREE.MeshBasicMaterial( { map: loader.load(  '/assets/cube3.png' ) } ),
-						new THREE.MeshBasicMaterial( { map: loader.load(  '/assets/cube4.png' ) } ),
-						new THREE.MeshBasicMaterial( { map: loader.load(  '/assets/cube5.png' ) } ),
-						new THREE.MeshBasicMaterial( { map: loader.load(  '/assets/cube6.png' ) } ),
+						new THREE.MeshBasicMaterial( { map: loader.load(  '../assets/cube1.png' ) } ),
+						new THREE.MeshBasicMaterial( { map: loader.load(  '../assets/cube2.png' ) } ),
+						new THREE.MeshBasicMaterial( { map: loader.load(  '../assets/cube3.png' ) } ),
+						new THREE.MeshBasicMaterial( { map: loader.load(  '../assets/cube4.png' ) } ),
+						new THREE.MeshBasicMaterial( { map: loader.load(  '../assets/cube5.png' ) } ),
+						new THREE.MeshBasicMaterial( { map: loader.load(  '../assets/cube6.png' ) } ),
 						
 		];
 		/*		
