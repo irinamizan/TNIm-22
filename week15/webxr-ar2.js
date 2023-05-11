1f145bc3fea650bcdc52de2a542523a153076b42
irinamizan <125154498+irinamizan@users.noreply.github.com>
2023-05-11 13:02:18 +0300

    Update webxr-ar2.js

--- week15/webxr-ar2.js
+++ week15/webxr-ar2.js
@@ -90,8 +90,8 @@
         
         modelloader.load( '../assets/birch_tree_-_low_poly.glb', function ( gltf ) {
                     scene.add(gltf.scene);
-                    gltf.scene.position.set(-2,0.2,0);
-                    gltf.scene.scale.set(0.0001,0.0001,0.0001);
+                    gltf.scene.position.set(-4,0.2,0);
+                    gltf.scene.scale.set(0.001,0.001,0.001);
         }, function ( xhr ) {
                 console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
         }, function ( error ) {
